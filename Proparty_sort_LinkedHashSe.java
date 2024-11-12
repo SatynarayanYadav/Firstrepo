600e6b2de7d8c9d44cb1e1e7caa375f5da73e24a
satya <satya27890@gmail.com>
2024-11-12 22:03:03 +0530

    Pushing java assignment code

---
+++ Proparty_sort_LinkedHashSe.java
@@ -0,0 +1,25 @@
+package assignment_from_27;
+
+import java.util.LinkedHashSet;
+
+public class Proparty_sort_LinkedHashSe 
+{
+  public static void main(String[] args) 
+  {
+	  LinkedHashSet LH =  new LinkedHashSet();	
+	  LH.add(89);
+	  LH.add(12);
+	  LH.add(90);
+	  LH.add("String1");
+	  LH.add("test");
+	  LH.add("test");
+	  LH.add(null);
+	  LH.add(null);
+	  LH.add('H');
+	  LH.add(18.78);
+	  LH.add(35.67f);
+	  LH.add('P');
+	   
+	   System.out.println(LH);
+  }
+}
---
+++ Remove_space_31.java
@@ -0,0 +1,14 @@
+package assignment_from_27;
+//Assignment 31 In the given String Ex;-"My Name is Java" remove all spacese involved using 
+//ReplaceAll function
+public class Remove_space_31 
+{
+   public static void main(String[] args) 
+   {
+	  String St = "My Name is Java";
+	  
+	 String R = St.replaceAll(" ", "");
+	 
+	 System.out.println(R);
+   }
+}
